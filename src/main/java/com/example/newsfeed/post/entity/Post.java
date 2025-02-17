295f5a604915fb7f77b82eabfc5d9b27235a5857
LJUNGM <dlwjdals3667@gmail.com>
2025-02-17 16:42:56 +0900

    [feat] 게시글 생성 및 수정 로그인이 되어있을 때 게시글을 작성하고 수정할 수 있습니다 게시글은 작성자 본인만 수정 가능합니다.

---
+++ src/main/java/com/example/newsfeed/post/entity/Post.java
@@ -0,0 +1,36 @@
+package com.example.newsfeed.post.entity;
+
+import com.example.newsfeed.common.entity.BaseEntity;
+import com.example.newsfeed.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "posts")
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Post(User user, String title, String content) {
+        this.user = user;
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
