1588d18358ba0e624a52a1a2f217c1aa7afca080
이지은 <tori@ryanlab.kr>
2025-02-18 14:48:07 +0900

    [refactor] RequestMapping friend -> friends 로 변경

--- src/main/java/com/example/newsfeed/friend/controller/FriendController.java
+++ src/main/java/com/example/newsfeed/friend/controller/FriendController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/friend")
+@RequestMapping("/friends")
 @RequiredArgsConstructor
 public class FriendController {
 
